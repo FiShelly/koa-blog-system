f9143734ab1dab01e0fd25933b4964641e8e683d
Fishelly <420006782@qq.com>
2018-08-03 13:15:08 +0800

    fix pagination component bug and add tag component.

--- front-end/blog-admin/src/app/components/tag/tag.component.ts
+++ front-end/blog-admin/src/app/components/tag/tag.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-tag',
-  templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.scss']
+    selector: 'app-tag',
+    templateUrl: './tag.component.html',
+    styleUrls: ['./tag.component.scss']
 })
 export class TagComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    
+    @Input() type: string = 'default';
+    @Input() leftIcon: string = '';
+    @Input() rightIcon: string = '';
+    @Input() isHover: boolean = false;
+    
+    @Output() leftIconClick: EventEmitter<any> = new EventEmitter<any>();
+    @Output() rightIconClick: EventEmitter<any> = new EventEmitter<any>();
+    
+    constructor() {
+    }
+    
+    ngOnInit() {
+    }
+    
+    handlerClick($e, type) {
+        $e.stopPropagation();
+        this[`${type}IconClick`].emit($e);
+    }
 }
