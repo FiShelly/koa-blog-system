2d0ef8587bc9ffdb0866336d27bf7205073a89ab
Fishelly <420006782@qq.com>
2018-08-09 14:10:57 +0800

    add event bus.

--- front-end/blog-admin/src/app/services/eventBus/event-bus.service.ts
+++ front-end/blog-admin/src/app/services/eventBus/event-bus.service.ts
@@ -1,14 +1,55 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Subject} from 'rxjs';
+import {filter} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
+import {Subscribable} from 'rxjs/src/internal/types';
+import {Subscription} from 'rxjs/src/internal/Subscription';
+
 
 /**
  * 事件总线，组件之间可以通过这个服务进行通讯
  */
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EventBusService {
-
-  constructor() { }
+    $bus = new Subject<Event>();
+    $busObs = this.$bus.asObservable();
+    _map: object = {};
+    
+    constructor() {
+    }
+    
+    emit(name: string, data: any) {
+        const event = new Event(name, data);
+        this.$bus.next(event);
+    }
+    
+    on(name: string, func: any) {
+        this._map[name] = this._map[name] ? this._map[name] : [];
+        const subscribe = this.$busObs.pipe(filter((event: Event) => event.type === name))
+            .subscribe(func);
+        this._map[name].push(subscribe);
+    }
+    
+    off(name) {
+        const subArray = this._map[name] || [];
+        subArray.forEach(val => {
+            val.unsubscribe();
+        });
+        delete this._map[name];
+    }
+   
 }
+
+export class Event {
+    type: string;
+    data: any;
+    
+    constructor(type, data) {
+        this.type = type;
+        this.data = data;
+    }
+}
+
