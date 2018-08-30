9ae3a7450d56b7c78997240f7cc93af96aa67687
Fishelly <420006782@qq.com>
2018-08-30 13:02:08 +0800

    no message

---
+++ config/server.config.exmaple.js
@@ -0,0 +1,18 @@
+const dbConfig = {
+    username: 'root',
+    password: 'root',
+    dbname: 'koa-blog',
+    options: {
+        'host': 'localhost',
+        'port': 3306,
+        'dialect': 'mysql',
+        'timezone': '+08:00',
+        'pool': {
+            'max': 3,
+            'min': 0,
+            'idle': 10000
+        }
+    }
+};
+
+module.exports = dbConfig;
