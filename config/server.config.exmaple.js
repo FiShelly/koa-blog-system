a2e3ac320a2b71955e84bf4c0b71d586259ab51f
Fishelly <420006782@qq.com>
2018-09-01 14:53:47 +0800

    no message

--- config/server.config.exmaple.js
+++ config/server.config.exmaple.js
@@ -2,42 +2,55 @@ const serverConfig = {
     prot: 3200,
     host: 'http://fishelly.top',
     log: {
-        'appenders': [
-            {
-                'type': 'console'
+        appenders: {
+            out: {
+                type: 'console'
             },
-            {
-                'type': 'clustered',
-                'appenders': [
-                    {
-                        'type': 'dateFile',
-                        'filename': 'startup.log',
-                        'pattern': '-yyyy-MM-dd',
-                        'category': 'startup'
-                    },
-                    {
-                        'type': 'dateFile',
-                        'filename': 'http.log',
-                        'pattern': '-yyyy-MM-dd',
-                        'category': 'http'
-                    },
-                    {
-                        'type': 'file',
-                        'filename': 'app.log',
-                        'maxLogSize': 10485760,
-                        'numBackups': 5
-                    },
-                    {
-                        'type': 'logLevelFilter',
-                        'level': 'ERROR',
-                        'appender': {
-                            'type': 'file',
-                            'filename': 'errors.log'
-                        }
-                    }
-                ]
+            http: {
+                type: 'dateFile',
+                filename: './logs/http',
+                pattern: '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            error: {
+                type: 'dateFile',
+                filename: './logs/error',
+                pattern: '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            app: {
+                'type': 'dateFile',
+                'filename': './logs/app',
+                pattern: '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            startup: {
+                'type': 'dateFile',
+                'filename': './logs/startup',
+                pattern: '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            default: {
+                type: 'dateFile',
+                filename: './logs/default',
+                'pattern': '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
+            },
+            mysql: {
+                type: 'dateFile',
+                filename: './logs/mysql',
+                'pattern': '-yyyy-MM-dd.log',
+                alwaysIncludePattern: true
             }
-        ]
+        },
+        categories: {
+            default: {appenders: ['out', 'default'], level: 'info'},
+            http: {appenders: ['http'], level: 'info'},
+            startup: {appenders: ['out', 'startup'], level: 'info'},
+            error: {appenders: ['error'], level: 'error'},
+            app: {appenders: ['app'], level: 'info'},
+            mysql: {appenders: ['mysql'], level: 'info'}
+        }
     }
 };
 
